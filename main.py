77655386a7032e3f9bb773a592fb3d420988758f
Yukang <yukanggogogo@gmail.com>
2025-05-17 19:28:24 +0800

    Added container

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from openai import OpenAI
 import io
 import base64
+from streamlit_extras.stylable_container import stylable_container
 
 load_dotenv()  
 
@@ -12,44 +13,53 @@ client = OpenAI(api_key=os.getenv("GPTKEY"))
 st.title("WHAT\'S THAT RASH?")
 st.write("Concerned? Let's find out what is that rash") 
 
-descList = st.multiselect("What is your rash like?", ["bumpy","rough", "dry", "red","white","clustered", "scaly", "blister", "crusty", "painful", "itchy", "warm", "tender", "hot","flaky", "scabbed", "burning", "tingly" ],None)
-st.write(descList)
+with stylable_container(
+key="test_button",
+# 'button' below in css_styles is the element you will be customizing.
+css_styles="""
+    { 
+        background-color: #BFE7F9;
+        border: none;
+        color: white;
+        border-radius: 25px;
+        padding: 35px;
+    }
+    """,
+):
+    descList = st.multiselect("What is your rash like?", ["bumpy","rough", "dry", "red","white","clustered", "scaly", "blister", "crusty", "painful", "itchy", "warm", "tender", "hot","flaky", "scabbed", "burning", "tingly" ],None)
+    def encode_image(image):
+        return base64.b64encode(image.read()).decode("utf-8")
 
-def encode_image(image):
-    return base64.b64encode(image.read()).decode("utf-8")
+    skinCondition = st.file_uploader("Upload a Picture of Your Skin Condition", type=["jpg", "jpeg", "png"])
 
-skinCondition = st.file_uploader("Upload a Picture of Your Skin Condition", type=["jpg", "jpeg", "png"])
-
-if skinCondition:
-    desc = ','.join(descList)
-    st.image(skinCondition, caption = "Uploaded image", use_container_width =True)
-    base64_image = encode_image(skinCondition)
-    if len(descList) > 0:
-        response = client.chat.completions.create(
-            model="gpt-4.1",
-            messages=[
-                {
-                    "role": "user", 
-                    "content": [
-                            { "type": "text", "text": "You are a dermatologist that assesses skin conditions" },
-                            { "type": "text", "text": "Within 500 characters, could you identify the image and create a report to highlight important details of the skin condition, what condition it most likely is, and in bullet points, provide medical recommendations. Make it simple for the average consumer to understand." },
-                            { "type": "text", "text": desc },
-                            {
-                                "type": "image_url",
-                                "image_url": {
-                                    "url": f"data:image/jpeg;base64,{base64_image}",
-                                    "detail": "low"
-                                },     
-                            },
-                    ]
-                }
-            ],
-            temperature = 0.0
-        )
-    else:
+    if skinCondition:
         st.image(skinCondition, caption = "Uploaded image", use_container_width =True)
         base64_image = encode_image(skinCondition)
+        
         if len(descList) > 0:
+            desc = ','.join(descList)
+            response = client.chat.completions.create(
+                model="gpt-4.1",
+                messages=[
+                    {
+                        "role": "user", 
+                        "content": [
+                                { "type": "text", "text": "You are a dermatologist that assesses skin conditions" },
+                                { "type": "text", "text": "Within 500 characters, could you identify the image and create a report to highlight important details of the skin condition, what condition it most likely is, and in bullet points, provide medical recommendations. Make it simple for the average consumer to understand." },
+                                { "type": "text", "text": desc },
+                                {
+                                    "type": "image_url",
+                                    "image_url": {
+                                        "url": f"data:image/jpeg;base64,{base64_image}",
+                                        "detail": "low"
+                                    },     
+                                },
+                        ]
+                    }
+                ],
+                temperature = 0.0
+            )
+        else:
             response = client.chat.completions.create(
                 model="gpt-4.1",
                 messages=[
@@ -70,22 +80,22 @@ if skinCondition:
                 ],
                 temperature = 0.0
             )
-        
-    st.markdown(response.choices[0].message.content)
+            
+        st.markdown(response.choices[0].message.content)
 
-    with st.container(border=True):
-        st.write("Send email with uploaded image and suggested diagnosis as saved PDF.") 
-        if st.button(label="Generate Report"):
-            image = image.convert("RGB")
-            pdf_bytes = io.BytesIO()
-            image.save(pdf_bytes, format="PDF")
-            pdf_bytes.seek(0)
-            st.success("PDF generated successfully!")
-            st.download_button(
-                label="Send",
-                data=pdf_bytes,
-                file_name="converted_image.pdf",
-                mime="application/pdf"
-            )
+        with st.container(border=True):
+            st.write("Send email with uploaded image and suggested diagnosis as saved PDF.") 
+            if st.button(label="Generate Report"):
+                image = image.convert("RGB")
+                pdf_bytes = io.BytesIO()
+                image.save(pdf_bytes, format="PDF")
+                pdf_bytes.seek(0)
+                st.success("PDF generated successfully!")
+                st.download_button(
+                    label="Send",
+                    data=pdf_bytes,
+                    file_name="converted_image.pdf",
+                    mime="application/pdf"
+                )
 
-        st.link_button("Send Gmail", url="https://mail.google.com/mail/?view=cm&fs=1&to=&su=Concerns%20About%20My%20Health&body=Please%20find%20the%20PDF%20document%20attached.%0A%0A%28You%20can%20manually%20attach%20the%20PDF%29%0A%0ABest%20Regards,")
+            st.link_button("Send Gmail", url="https://mail.google.com/mail/?view=cm&fs=1&to=&su=Concerns%20About%20My%20Health&body=Please%20find%20the%20PDF%20document%20attached.%0A%0A%28You%20can%20manually%20attach%20the%20PDF%29%0A%0ABest%20Regards,")

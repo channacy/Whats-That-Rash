41409843c9c93e6310960c47661174da3b9c75e5
Dylan <dylan@Dylans-MacBook-Air.local>
2025-05-17 18:13:43 +0800

    gpt integration v2

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if skinCondition:
                 "role": "user", 
                 "content": [
                     { "type": "text", "text": "You are a dermatologist that assesses skin conditions" },
-                    { "type": "text", "text": "Could you describe the image and create a report to highlight important details of the skin condition, and provide medical recommendations. Create a report with observations, important details and recommendations with bullet points." },
+                    { "type": "text", "text": "Within 500 characters, could you identify the image and create a report to highlight important details of the skin condition, what condition it most likely is, and in bullet points, provide medical recommendations. Make it simple for the average consumer to understand." },
                     {
                         "type": "image_url",
                         "image_url": {
@@ -41,36 +41,3 @@ if skinCondition:
     )
     
     st.markdown(response.choices[0].message.content)
-
-# if "WTR" not in st.session_state:
-#     st.session_state["openai_model"] = "gpt-3.5-turbo"
-
-# if "messages" not in st.session_state:
-#     st.session_state.messages = []
-
-# for message in st.session_state.messages:
-#     with st.chat_message(message["role"]):
-#         st.markdown(message["content"])
-
-# if prompt := st.chat_input("How can we help you today?"):
-#     response = client.responses.create(
-#         model="gpt-4.1",
-#         input="Write a one-sentence bedtime story about a unicorn."
-#     )
-
-#     st.title("WHAT\'S THAT RASH?")
-#     st.write("Concerned? Let's find out what is that rash") 
-#     response = client.responses.create(
-#         model="gpt-4.1-mini",
-#         input=[{
-#             "role": "user",
-#             "content": [
-#                 {"type": "input_text", "text": "what's in this image?"},
-#                 {
-#                     "type": "input_image",
-#                     "image_url": "https://www.minecraftskins.com/uploads/preview-skins/2025/03/25/my-first-minecraft-youtube-skin-extended-version--23141318.png?v825",
-#                 },
-#             ],
-#         }],
-#     )
-

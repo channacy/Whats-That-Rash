69ad049ef755edb4917b8e0f60171a289a077033
Dylan <dylan@Dylans-MacBook-Air.local>
2025-05-17 18:02:02 +0800

    GPT integration

--- main.py
+++ main.py
@@ -1,41 +1,76 @@
 import streamlit as st
-import cv2
 import os
-import numpy as np
-from PIL import Image
-import requests
 from dotenv import load_dotenv
-
-load_dotenv()  
+import base64
 from openai import OpenAI
-client = OpenAI(api_key=os.getenv("GPTKEY"))
 
-# response = client.responses.create(
-#     model="gpt-4.1",
-#     input="Write a one-sentence bedtime story about a unicorn."
-# )
+load_dotenv()
+client = OpenAI(api_key = os.getenv("GPTKEY"))
 
-st.title("WHAT\'S THAT RASH?")
-st.write("Concerned? Let's find out what is that rash") 
-response = client.responses.create(
-    model="gpt-4.1-mini",
-    input=[{
-        "role": "user",
-        "content": [
-            {"type": "input_text", "text": "what's in this image?"},
+st.title("What's That Rash?")
+
+def encode_image(image):
+    return base64.b64encode(image.read()).decode("utf-8")
+
+skinCondition = st.file_uploader("Upload a Picture of Your Skin Condition", type=["jpg", "jpeg", "png"])
+
+if skinCondition:
+    st.image(skinCondition, caption = "Uploaded image", use_container_width =True)
+
+    base64_image = encode_image(skinCondition)
+
+    response = client.chat.completions.create(
+        model="gpt-4.1",
+        messages=[
             {
-                "type": "input_image",
-                "image_url": "https://www.minecraftskins.com/uploads/preview-skins/2025/03/25/my-first-minecraft-youtube-skin-extended-version--23141318.png?v825",
-            },
+                "role": "user", 
+                "content": [
+                    { "type": "text", "text": "You are a dermatologist that assesses skin conditions" },
+                    { "type": "text", "text": "Could you describe the image and create a report to highlight important details of the skin condition, and provide medical recommendations. Create a report with observations, important details and recommendations with bullet points." },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}",
+                            "detail": "low"
+                        },     
+                    },
+                ]
+            }
         ],
-    }],
-)
+        temperature = 0.0
+    )
+    
+    st.markdown(response.choices[0].message.content)
 
-# print(response.output_text)
+# if "WTR" not in st.session_state:
+#     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-st.write(response.output_text)
+# if "messages" not in st.session_state:
+#     st.session_state.messages = []
 
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-if uploaded_file:
-    image = Image.open(uploaded_file)
-    st.image(image, use_container_width =True)
+# for message in st.session_state.messages:
+#     with st.chat_message(message["role"]):
+#         st.markdown(message["content"])
+
+# if prompt := st.chat_input("How can we help you today?"):
+#     response = client.responses.create(
+#         model="gpt-4.1",
+#         input="Write a one-sentence bedtime story about a unicorn."
+#     )
+
+#     st.title("WHAT\'S THAT RASH?")
+#     st.write("Concerned? Let's find out what is that rash") 
+#     response = client.responses.create(
+#         model="gpt-4.1-mini",
+#         input=[{
+#             "role": "user",
+#             "content": [
+#                 {"type": "input_text", "text": "what's in this image?"},
+#                 {
+#                     "type": "input_image",
+#                     "image_url": "https://www.minecraftskins.com/uploads/preview-skins/2025/03/25/my-first-minecraft-youtube-skin-extended-version--23141318.png?v825",
+#                 },
+#             ],
+#         }],
+#     )
+

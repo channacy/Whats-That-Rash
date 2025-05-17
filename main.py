35617ecb93ee2f7c79a58f7599cbc6d8773c58ab
Yukang <yukanggogogo@gmail.com>
2025-05-17 16:13:39 +0800

    UI cleanup

--- main.py
+++ main.py
@@ -14,16 +14,11 @@ client = OpenAI(api_key=os.getenv("GPTKEY"))
 #     model="gpt-4.1",
 #     input="Write a one-sentence bedtime story about a unicorn."
 # )
-st.write(os.getenv("GPTKEY"))
-st.write("Streamlit is also great for more traditional ML use cases like computer vision or NLP. Here's an example of edge detection using OpenCV. 👁️") 
+
+st.title("WHAT\'S THAT RASH?")
+st.write("Concerned? Let's find out what is that rash") 
 
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 if uploaded_file:
     image = Image.open(uploaded_file)
-else:
-    image = Image.open(requests.get("https://picsum.photos/200/120", stream=True).raw)
-
-edges = cv2.Canny(np.array(image), 100, 200)
-tab1, tab2 = st.tabs(["Detected edges", "Original"])
-tab1.image(edges, use_column_width=True)
-tab2.image(image, use_column_width=True)
+    st.image(image, use_container_width =True)
